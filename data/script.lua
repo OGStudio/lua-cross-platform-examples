dd847b7526dc5e09034cc9dec7d65b2e5a33f2cb
Michael Kapelko <kornerr@gmail.com>
2018-10-11 17:13:13 +0300

    Introduce application.camera.clearColor into Lua

--- data/script.lua
+++ data/script.lua
@@ -1,5 +1,31 @@
 
+application = {}
+
+camera = {}
+application.camera = camera
+
+-- clearColor property.
+local clearColorMT =
+{
+    -- Getter.
+    __index = function(table, key)
+        if (key == "clearColor") then
+            return ENV:call("application.camera.clearColor", {})
+        end
+        return nil
+    end,
+    -- Setter.
+    __newindex = function(table, key, value)
+        if (key == "clearColor") then
+            ENV:call("application.camera.clearColor", value)
+        end
+    end,
+}
+setmetatable(camera, clearColorMT)
+
 print("Set background color")
 
-ENV:call("application.camera.clearColor", {0.5, 0.5, 0.5})
+local color = application.camera.clearColor
+print("Current bg color", color[1], color[2], color[3])
+application.camera.clearColor = {0.5, 0.5, 0.5}
 
